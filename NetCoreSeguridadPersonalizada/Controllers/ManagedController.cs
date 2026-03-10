d02bd69206399dba0fe96105f8d80e3d30e04657
alooonsogm <alonsogarciamartin2@gmail.com>
2026-03-10 11:46:08 +0100

    Add project files.

---
+++ NetCoreSeguridadPersonalizada/Controllers/ManagedController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace NetCoreSeguridadPersonalizada.Controllers
+{
+    public class ManagedController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string username, string password)
+        {
+            if(username.ToLower() == "admin" && password == "12345")
+            {
+                //Por medidas de seguridad se genera una Cookie cifrada que es para sabr si el user se ha validado en este explorador.
+                ClaimsIdentity identity = new ClaimsIdentity(
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    ClaimTypes.Name, ClaimTypes.Role
+                    );
+                //Un Claim es informacion del usuario.
+                Claim claimUsername = new Claim(ClaimTypes.Name, username);
+                Claim claimRole = new Claim(ClaimTypes.Role, "USUARIO");
+                identity.AddClaim(claimUsername);
+                identity.AddClaim(claimRole);
+                //Creamos un Usuario principal con esta identidad.
+                ClaimsPrincipal userPrincipal = new ClaimsPrincipal(identity);
+                //Damos de alta al user en el sistema.
+                await HttpContext.SignInAsync(
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    userPrincipal,
+                    new AuthenticationProperties
+                    {
+                        ExpiresUtc = DateTime.Now.AddMinutes(10)
+                    });
+                return RedirectToAction("Perfil", "Usuarios");
+            }
+            else
+            {
+                ViewData["MENSAJE"] = "Credenciales incorrectas.";
+                return View();
+            }
+        }
+    }
+}
---
+++ NetCoreSeguridadPersonalizada/Filters/AuthorizeUsersAttribute.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace NetCoreSeguridadPersonalizada.Filters
+{
+    public class AuthorizeUsersAttribute : AuthorizeAttribute, IAuthorizationFilter
+    {
+        //Este metodo es el que permitira impedir el acceso a los actions/controllers.
+        //El filter simplemente se encarga de interceptar peticions y decicir que hacer.
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            //El usuario que se ha validado en nuestra app estara dentro del context y en una propiedad llamada User.
+            //Cualquier user esta compuesto por dos caracterisiticas:
+            //1) Identity: El nombre del usuario y si es activo.
+            //2) Principal: El rol del usuario.
+            var user = context.HttpContext.User;
+            //El fintro solamente preguntará si existe el user, y solo entra en accion sino existe.
+            if(user.Identity.IsAuthenticated == false)
+            {
+                //lo llevamos al Log in sino se ha autenticado.
+                //A la ruta debemos enviarle un controller y un action.
+                RouteValueDictionary rutaLogIn = new RouteValueDictionary(new { controller = "Managed", action = "Login" });
+                //Devolvemos la peticion al Log In.
+                context.Result = new RedirectToRouteResult(rutaLogIn);
+            }
+        }
+    }
+}
